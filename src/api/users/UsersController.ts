0877e2d8e70b93e750c89a1f7e28f18955d3f5e7
Cody Fingerson <codyfingerson0428@gmail.com>
2025-02-05 00:46:27 -0700

    chore: Refactor UserController to include create, update, delete, and get user functionalities with DTOs

--- src/api/users/UsersController.ts
+++ src/api/users/UsersController.ts
@@ -1,11 +1,38 @@
 import { Request, Response } from "express";
 import UserService from "./UsersService";
+import { CreateUserDto, UpdateUserDto } from "./UsersDto";
 
 export default class UserController {
     static async createUser(req: Request, res: Response) {
         try {
-            const user = await UserService.createUser(req.body);
+            const { isActive = true, roleId, name, username, email, password } = req.body;
+            const data: CreateUserDto = { isActive, roleId, name, username, email, password };
+            const user = await UserService.createUser(data);
+
             res.status(201).json(user);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    static async updateUser(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const { isActive, roleId, name, username, email, password } = req.body;
+            const data: UpdateUserDto = { isActive, roleId, name, username, email, password };
+            const user = await UserService.updateUser(id, data);
+
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    static async getAllUsers(req: Request, res: Response) {
+        try {
+            const users = await UserService.getAllUsers();
+
+            res.status(200).json(users);
         } catch (error) {
             res.status(400).json({ error: error.message });
         }
@@ -13,19 +40,23 @@ export default class UserController {
 
     static async getUserById(req: Request, res: Response) {
         try {
-            const user = await UserService.getUserById(req.params.id);
+            const { id } = req.params;
+            const user = await UserService.getUserById(id);
+
             res.status(200).json(user);
         } catch (error) {
-            res.status(404).json({ error: error.message });
+            res.status(400).json({ error: error.message });
         }
     }
 
-    static async getAllUsers(req: Request, res: Response) {
+    static async deleteUser(req: Request, res: Response) {
         try {
-            const users = await UserService.getAllUsers(req.query.sortBy as string, req.query.order as 'asc' | 'desc');
-            res.status(200).json(users);
+            const { id } = req.params;
+            await UserService.deleteUser(id);
+
+            res.status(204).end();
         } catch (error) {
-            res.status(500).json({ error: error.message });
+            res.status(400).json({ error: error.message });
         }
     }
 }
