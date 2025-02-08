2e3c7cd13b7ef063435b45f058094f45630e57b7
Cody Fingerson <codyfingerson0428@gmail.com>
2025-02-07 23:32:07 -0700

    refactor: Simplify error middleware functions by removing unnecessary line breaks

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,21 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
-export function errorLogger(
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function errorLogger(err: any, req: Request, res: Response, next: NextFunction) {
   console.error(err);
   next(err); // Pass error to the next middleware
 }
 
-export function errorResponder(
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function errorResponder(err: any, req: Request, res: Response, next: NextFunction) {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
