4b1fd7a4bf5b5fe888600cf1bae4861e4909dc2b
Cody Fingerson <codyfingerson0428@gmail.com>
2025-03-22 00:25:50 -0600

    feat: implement ProductCategoriesController and ProductsCategoriesDto for product category management

---
+++ src/api/product-categories/ProductsCategoriesController.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from "express";
+import BaseController from "../BaseController";
+import ProductCategoriesService from "./ProductsCategoriesService";
+
+class ProductCategoriesController extends BaseController<ProductCategoriesService> {
+    constructor(productCategoriesService: ProductCategoriesService) {
+        super(productCategoriesService);
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        this.handleRequest(() => this.service.createProductCategory(req.body), res, next, 201);
+    }
+
+    async getAll(req: Request, res: Response, next: NextFunction) {
+        this.handleRequest(() => this.service.getProductCategories(), res, next);
+    }
+
+    async getById(req: Request, res: Response, next: NextFunction) {
+        const { id } = req.params;
+        this.handleRequest(() => this.service.getProductCategoryById(id), res, next);
+    }
+
+    async update(req: Request, res: Response, next: NextFunction) {
+        this.handleRequest(() => this.service.updateProductCategory(req.params.id, req.body), res, next);
+    }
+
+    async delete(req: Request, res: Response, next: NextFunction) {
+        await this.handleRequest(() => this.service.deleteProductCategory(req.params.id), res, next, 204);
+    }
+}
+
+export default ProductCategoriesController;
