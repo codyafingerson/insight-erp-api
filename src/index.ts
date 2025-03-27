d4a63b4a5440408c4c2c06c802a2f3e50d99c0b2
Cody Fingerson <codyfingerson0428@gmail.com>
2025-03-26 23:42:01 -0600

    feat: add OrdersRoutes to main server for order management API

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import passport from "./config/passport";
 import errorHandler from "./middlewares/errorMiddleware";
 import { RedisStore } from "connect-redis";
 import { redisClient } from "./config/redis";
+import { morganStream } from "./utils/logger";
 
 // Controllers
 import RolesRoutes from "./api/roles/RolesRoutes";
@@ -18,7 +19,7 @@ import DepartmentsRoutes from "./api/departments/DepartmentsRoutes";
 import EmployeesRoutes from "./api/employees/EmployeesRoutes";
 import ProductCategoriesRoutes from "./api/product-categories/ProductsCategoriesRoutes";
 import ProductRoutes from "./api/products/ProductsRoutes";
-import { morganStream } from "./utils/logger";
+import OrdersRoutes from "./api/orders/OrdersRoutes.ts";
 
 class Server {
     private app: Application;
@@ -122,6 +123,7 @@ class Server {
         this.app.use("/api/employees", EmployeesRoutes);
         this.app.use("/api/product-categories", ProductCategoriesRoutes);
         this.app.use("/api/products", ProductRoutes);
+        this.app.use("/api/orders", OrdersRoutes);
     }
 }
 
